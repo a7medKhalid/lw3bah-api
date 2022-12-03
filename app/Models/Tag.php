0765b8c7a9d640d5d4ec801261553695c6ab3e32
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-12-03 18:50:06 +0300

    configure mongodb

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CreateNewCourse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('create-new-course', CreateNewCourse::class);
+});
+
+
+//guest routes
+
