1868090715e774a6821d1f5c047056e52a85c50a
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-12-10 18:52:42 +0300

    add role to register

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -16,6 +16,7 @@ class RegistrationTest extends TestCase
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'role' => 'student',
         ]);
 
         $this->assertAuthenticated();
