fcdeaaf1287e7d7bd5f0778669cc23c7a416846e
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-12-13 05:00:02 +0300

    fixes

--- app/Http/Controllers/TeacherViewSlides.php
+++ app/Http/Controllers/TeacherViewSlides.php
@@ -30,15 +30,9 @@ class TeacherViewSlides extends Controller
             ], 403);
         }
 
-        //validate request
-        $request->validate([
-            'slide_id' => 'required',
-        ]);
 
         //authorize request
-        $user = $request->user();
-        $slide = Slide::find($request->slide_id);
-        $course = $slide->lesson->section->course;
+        $course = $lesson->section->course;
 
         if(!$user->can('viewAsTeacher', $course)){
             return response()->json([
