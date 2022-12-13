ff0c85c9c8eead8cdb0245882ee3d1902bdaa625
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-12-13 21:38:55 +0300

    fix update slide order

--- app/Http/Controllers/UpdateSlideOrder.php
+++ app/Http/Controllers/UpdateSlideOrder.php
@@ -40,14 +40,34 @@ class UpdateSlideOrder extends Controller
             ], 403);
         }
 
-        //update slide order
-        $slide = $slide->update([
-            'order' => $request->order,
-        ]);
 
-        $slide->save();
 
-        return $slide;
+        //get slides
+
+        $slides = $lesson->slides()->get()->toArray();
+
+        //swap slide place to new order
+        $slideWithUpdatedOrder = $slides[array_search($slide->_id, array_column($slides, '_id'))];
+        $slideToSwap = $slides[array_search($request->order, array_column($slides, 'order'))];
+
+        //remove slide with updated order
+        $slides = array_filter($slides, function($item) use ($slideWithUpdatedOrder){
+            return $item['_id'] != $slideWithUpdatedOrder['_id'];
+        });
+
+        //insert slide with updated order after slide to swap
+        array_splice($slides, array_search($slideToSwap['_id'], array_column($slides, '_id')) + 1, 0, [$slideWithUpdatedOrder]);
+
+        //update order of all slides
+        foreach ($slides as $key => $slide){
+            $slide = Slide::find($slide['_id']);
+            $slide->update([
+                'order' => $key + 1,
+            ]);
+            $slide->save();
+        }
+
+        return $lesson->slides()->get();
 
 
     }
